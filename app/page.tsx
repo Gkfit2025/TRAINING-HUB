96fe60218bf0d9ea500a439e0a2ccf75faf8772a
Gkfit2025 <promotiongkf46@gmail.com>
2025-08-22 16:45:35 +0530

    logo.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,17 @@ export default function HomePage() {
 
       <div className="relative z-10">
         <Header />
-
+<header className="bg-orange-700 text-white py-5 text-center sticky top-0 z-50">
+        <div className="flex items-center justify-center gap-4 mb-2">
+          <Image
+            src="/logo.jpeg"
+            alt="Logo"
+            width={60}
+            height={60}
+            className="rounded-full"
+          />
+          </div>
+      </header>
         <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
           <div className="flex flex-col lg:flex-row items-center justify-between gap-12">
             {/* Left side - Title and Button */}
