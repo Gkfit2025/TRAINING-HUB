a8def8164a21c8bc564e4948b2f2318075cfa34d
Gkfit2025 <promotiongkf46@gmail.com>
2025-08-26 18:32:07 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function HomePage() {
             .marquee {
               display: inline-block;
               white-space: nowrap;
-              animation: marquee 15s linear infinite;
+              animation: marquee 20s linear infinite;
             }
             @keyframes marquee {
               0% {
@@ -43,7 +43,7 @@ export default function HomePage() {
               className="text-base sm:text-lg lg:text-xl font-bold py-2 px-4"
               style={{ color: "#0000e6" }}
             >
-              Gift Skin – Gift Life தாய்ப்பால் தானம் செய்வீர்! குழந்தை நலன் காப்பீர்! எதிர்காலத்தை ஒளிரச் செய்ய சிறிய துளிகள்… Human Milk bank - Little drops to light the future… Donate excess breast milk and help nourish underweight infants in Mazhalai Illam.
+              ★ Gift Skin – Gift Life ★ தாய்ப்பால் தானம் செய்வீர்! குழந்தை நலன் கா�ப்பீர்! எதிர்காலத்தை ஒளிரச் செய்ய சிறிய துளிகள்… Human Milk bank - Little drops to light the future… Donate excess breast milk and help nourish underweight infants in Mazhalai Illam.
             </p>
           </div>
         </div>
