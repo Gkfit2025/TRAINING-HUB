331a2aab4b0d07f93a9ce32d0b80dbb6526d7798
Gkfit2025 <promotiongkf46@gmail.com>
2025-08-29 09:24:49 +0530

    Update header.tsx logo

--- components/navigation/header.tsx
+++ components/navigation/header.tsx
@@ -49,7 +49,7 @@ export function Header({ title, subtitle, moduleType }: HeaderProps) {
               {/* Logo image added here */}
               <Image
                 src="/logo12.png"
-                alt="Logo"
+                alt="Logo12"
                 width={40}
                 height={40}
                 className="rounded-lg"
